c0a2e0adcd614bbfac96cd2c64aec042da3179e9
mrum13 <muhammadrum135@gmail.com>
2022-05-25 22:36:21 +0800

    ini screen 2

--- app/src/main/java/com/rum/suitmedia/Screen2Activity.java
+++ app/src/main/java/com/rum/suitmedia/Screen2Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class Screen2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_screen2);
     }
 }
